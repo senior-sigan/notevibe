7b370c5c87b55da11bc9548da146c781f7db6269
Ilya Siganov <ilya.blan4@gmail.com>
2025-08-22 01:13:33 +0600

    vertical notes layouting

--- app/src/components/NotesList.tsx
+++ app/src/components/NotesList.tsx
@@ -50,7 +50,7 @@ export const NotesList: React.FC = () => {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="flex flex-col gap-6">
       {data.notes.map((note) => (
         <NoteCard key={note.id} note={note} />
       ))}
